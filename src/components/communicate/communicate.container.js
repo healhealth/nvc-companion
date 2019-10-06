e0fee68fa98314e629b238620b5657d3de539d38
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-10-06 15:04:34 +0200

    Started on Communicate module

--- src/components/communicate/communicate.container.js
+++ src/components/communicate/communicate.container.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Communicate = () => (
-  <p>Communicate</p>
+  <>
+    <h1>What would you like to do?</h1>
+    <button type='button'>Make a request</button>
+    <button type='button'>Express gratitude</button>
+    <button type='button'>Offer empathy</button>
+  </>
 )
 
 export default Communicate
