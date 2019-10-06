57e098a9725d2f4567bc5bbd77c6b39dda9b0df8
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-10-06 16:00:01 +0200

    Added some content to Communicate module

--- src/components/communicate/communicate.container.js
+++ src/components/communicate/communicate.container.js
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 import React, {useState} from 'react'
 
 const modules = {
@@ -18,6 +19,36 @@ const Communicate = () => {
         >
           Go back
         </button>
+
+        <h2>Make an observation</h2>
+        <p>When I [see|hear|remember|imagine|add your own] __,</p>
+        <ul>
+          <li>Is it factual?</li>
+          <li>Is it specific?</li>
+          <li>Is it free of judgement?</li>
+        </ul>
+
+        <h2>Identify the feeling</h2>
+        <p>I feel [list of emotions|add your own],</p>
+        <ul>
+          <li>Is it a feeling rather than a thought?</li>
+          <li>Is this really the feeling which was stimulated?</li>
+        </ul>
+
+        <h2>Identify the need</h2>
+        <p>Because my need for [list of needs|add your own] is not met.</p>
+        <ul>
+          <li>Is it a really a need?</li>
+          <li>Is this really the need behind the feeling?</li>
+        </ul>
+
+        <h2>Make a request</h2>
+        <p>Would you be willing to __ so __?</p>
+        <ul>
+          <li>Is this a request rather than a demand?</li>
+          <li>Is it specific?</li>
+          <li>Would the thing that I am requesting enrich my life?</li>
+        </ul>
       </>
     )
   }
