a27d40c7d936bf4e2d7a77b208ee7ad201691a2d
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-10-06 15:50:23 +0200

    Implemented basic navigation for communication

--- src/components/communicate/communicate.container.js
+++ src/components/communicate/communicate.container.js
@@ -1,12 +1,75 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Communicate = () => (
-  <>
-    <h1>What would you like to do?</h1>
-    <button type='button'>Make a request</button>
-    <button type='button'>Express gratitude</button>
-    <button type='button'>Offer empathy</button>
-  </>
-)
+const modules = {
+  makeARequest: 'makeARequest',
+  offerEmpathy: 'offerEmpathy',
+  expressGratitude: 'expressGratitude'
+}
+
+const Communicate = () => {
+  const [selectedModule, selectModule] = useState(null)
+  if (selectedModule === modules.makeARequest) {
+    return (
+      <>
+        <h1>Make a request</h1>
+        <button
+          type='button'
+          onClick={() => selectModule(null)}
+        >
+          Go back
+        </button>
+      </>
+    )
+  }
+  if (selectedModule === modules.offerEmpathy) {
+    return (
+      <>
+        <h1>Offer empathy</h1>
+        <button
+          type='button'
+          onClick={() => selectModule(null)}
+        >
+          Go back
+        </button>
+      </>
+    )
+  }
+  if (selectedModule === modules.expressGratitude) {
+    return (
+      <>
+        <h1>Express gratitude</h1>
+        <button
+          type='button'
+          onClick={() => selectModule(null)}
+        >
+          Go back
+        </button>
+      </>
+    )
+  }
+  return (
+    <>
+      <h1>What would you like to do?</h1>
+      <button
+        type='button'
+        onClick={() => selectModule(modules.makeARequest)}
+      >
+        Make a request
+      </button>
+      <button
+        type='button'
+        onClick={() => selectModule(modules.offerEmpathy)}
+      >
+        Offer empathy
+      </button>
+      <button
+        type='button'
+        onClick={() => selectModule(modules.expressGratitude)}
+      >
+        Express gratitude
+      </button>
+    </>
+  )
+}
 
 export default Communicate
