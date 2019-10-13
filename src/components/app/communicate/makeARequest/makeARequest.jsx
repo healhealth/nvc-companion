03bc433b5f42c0e8dd1d1312fce68461c1524eaf
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-10-13 23:19:11 +0200

    Added wrapper

--- src/components/app/communicate/makeARequest/makeARequest.jsx
+++ src/components/app/communicate/makeARequest/makeARequest.jsx
@@ -30,77 +30,79 @@ const MakeARequest = () => {
         {getStepTitle({step: currentStep})}
       </MainHeader>
       <div className='makeARequest'>
-        <div
-          className={
-            classNames({
-              current: currentStep === observation,
-              done: currentStep !== observation
-            })
-          }
-        >
-          <p>When I [see|hear|remember|imagine|add your own] __,</p>
-          <button
-            type='button'
-            onClick={() => setStep(feeling)}
-            disabled={currentStep !== observation}
+        <div className='steps'>
+          <div
+            className={
+              classNames('step', {
+                current: currentStep === observation,
+                done: currentStep !== observation
+              })
+            }
           >
-            Next
-          </button>
-        </div>
-        <div
-          className={
-            classNames({
-              current: currentStep === feeling,
-              done: ![observation, feeling].includes(currentStep),
-              upcoming: currentStep === observation
-            })
-          }
-        >
-          <p>I feel [list of emotions|add your own],</p>
-          <button
-            type='button'
-            onClick={() => setStep(need)}
-            disabled={currentStep !== feeling}
+            <p>When I [see|hear|remember|imagine|add your own] __,</p>
+            <button
+              type='button'
+              onClick={() => setStep(feeling)}
+              disabled={currentStep !== observation}
+            >
+              Next
+            </button>
+          </div>
+          <div
+            className={
+              classNames('step', {
+                current: currentStep === feeling,
+                done: ![observation, feeling].includes(currentStep),
+                upcoming: currentStep === observation
+              })
+            }
           >
-            Next
-          </button>
-        </div>
-        <div
-          className={
-            classNames({
-              current: currentStep === need,
-              done: ![observation, feeling, need].includes(currentStep),
-              upcoming: [observation, feeling].includes(currentStep)
-            })
-          }
-        >
-          <p>Because my need for [list of needs|add your own] is not met.</p>
-          <button
-            type='button'
-            onClick={() => setStep(request)}
-            disabled={currentStep !== need}
+            <p>I feel [list of emotions|add your own],</p>
+            <button
+              type='button'
+              onClick={() => setStep(need)}
+              disabled={currentStep !== feeling}
+            >
+              Next
+            </button>
+          </div>
+          <div
+            className={
+              classNames('step', {
+                current: currentStep === need,
+                done: ![observation, feeling, need].includes(currentStep),
+                upcoming: [observation, feeling].includes(currentStep)
+              })
+            }
           >
-            Next
-          </button>
-        </div>
-        <div
-          className={
-            classNames({
-              current: currentStep === request,
-              upcoming: currentStep !== request
-            })
-          }
-        >
-          <p>Would you be willing to __ so __?</p>
-          <button
-            type='button'
-            onClick={() => {
-              console.log('Nice job')
-            }}
-            disabled={currentStep !== request}
+            <p>Because my need for [list of needs|add your own] is not met.</p>
+            <button
+              type='button'
+              onClick={() => setStep(request)}
+              disabled={currentStep !== need}
+            >
+              Next
+            </button>
+          </div>
+          <div
+            className={
+              classNames('step', {
+                current: currentStep === request,
+                upcoming: currentStep !== request
+              })
+            }
           >
-            Finish
-          </button>
+            <p>Would you be willing to __ so __?</p>
+            <button
+              type='button'
+              onClick={() => {
+                console.log('Nice job')
+              }}
+              disabled={currentStep !== request}
+            >
+              Finish
+            </button>
+          </div>
         </div>
       </div>
     </>
